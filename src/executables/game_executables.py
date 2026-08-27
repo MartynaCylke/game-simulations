217de866d954e9c9ac18dd514317bd274174b768
MartynaCylke <101675374+MartynaCylke@users.noreply.github.com>
2026-08-27 19:13:51 +0200

    Fix simulations and remove committed virtualenv

---
+++ src/executables/game_executables.py
@@ -0,0 +1,22 @@
+from src.executables.executables import run_single_game
+
+def run_game_simulation(
+    game: str,
+    sims: int = 10,
+    target_rtp: float = 0.96,
+    books_path=None,
+    lookup_path=None,
+):
+    """
+    Uruchamia symulację danej gry.
+    :param game: "lines", "ways", "cluster" lub "scatter"
+    :param sims: liczba symulacji
+    :param target_rtp: docelowe RTP (0.96 = 96%)
+    """
+    return run_single_game(
+        game=game,
+        sims=sims,
+        target_rtp=target_rtp,
+        books_path=books_path,
+        lookup_path=lookup_path,
+    )
